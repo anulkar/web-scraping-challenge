75e24411c8b4eaae95e8db330c8928b6e3d96d0e
Atul Nulkar <nulkar@gmail.com>
2020-03-10 19:45:34 -0400

    Stored scrapted data to dict, added print logs + 2 sec sleep timers

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,15 +1,20 @@
 # Dependencies
 from splinter import Browser
-from bs4 import BeautifulSoup, UnicodeDammit
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-import requests
+import time
 import pymongo
 import pandas as pd
+import json
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning) 
 
 def scrape():
     # ------------------------------------------------------------------------------------
     # Scrape the NASA Mars News Site and retrieve the latest News Title and Paragraph Text
     # ------------------------------------------------------------------------------------
+    print("\nSTARTED Web Scraping the NASA Mars News Site...Hang Tight!")
+    print("------------------------------------------------------------")
 
     # To use splinter create a new Chrome headless Browser instance
     executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
@@ -20,6 +25,9 @@ def scrape():
 
     # Visit the Nasa Mars News Site
     browser.visit(nasa_mars_news_url)
+
+    # Add a 2 second sleep timer to allow the browser to finish loading the URL
+    time.sleep(2)
 
     # Get the HTML content of the visited page
     nasa_mars_news_html = browser.html
@@ -37,20 +45,22 @@ def scrape():
         news_title = news_listing.find('div', class_='content_title').find('a').text
         news_teaser = news_listing.find('div', class_='article_teaser_body').text
         # Print the retrieved information to console
-        print(f"Here's the latest article from the Nasa Mars News site:")
-        print("--------------------------------------------------------")
+        print(f"Scraped the latest news from the Nasa Mars News site:")
+        print("------------------------------------------------------")
         print(f"News Title: {news_title}")
         print(f"News Teaser: {news_teaser}")
-        print("--------------------------------------------------------")
+        print("------------------------------------------------------\n")
     except AttributeError as e:
         print(e)
 
+    # Store the scraped news and teaser into a dictionary
     mars_scraped_data = {"news_title": news_title, "news_teaser": news_teaser}
 
     # -----------------------------------------------------------------------------------------
     # Find the image URL for the current Featured Mars Image on the JPL Mars Space Images page
     # -----------------------------------------------------------------------------------------
 
+    print("Now Scraping JPL Mars Space Images Site for the Featured Image...Hang Tight!")
     # URL of JPL Mars Space Images Site that we want to scrape
     jpl_mars_images_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 
@@ -58,6 +68,9 @@ def scrape():
     browser = Browser('chrome', **executable_path, headless=False)
     browser.visit(jpl_mars_images_url)
 
+    # Add a 2 second sleep timer to allow the browser to finish loading the URL
+    time.sleep(2)
+    
     # Get the HTML content of the visited page
     jpl_mars_images_html = browser.html
 
@@ -73,8 +86,9 @@ def scrape():
     # Parse the JPL website URL and save a complete url string for the feature image
     parsed_uri = urlparse(jpl_mars_images_url)
     featured_image_url = f'{parsed_uri.scheme}://{parsed_uri.netloc}{featured_image_href}'
-    print(f"Found the following Featured Image on the JPL Mars Space Images site: {featured_image_url}")
+    print(f"Scraped this Featured Image on the JPL Mars Space Images site:\n{featured_image_url}\n")
 
+    # Stpre the scraped image URL into a dictionary
     mars_scraped_data["featured_image"] = featured_image_url
 
     # ------------------------------------------------------------------------------------
@@ -82,12 +96,17 @@ def scrape():
     # Scrape the latest Mars weather tweet containing the weather report tweet text
     # ------------------------------------------------------------------------------------
 
+    print("Now Scraping Mars Weather Tweets...Hang Tight!")
+
     # URL of Twitter page to be scraped
     mars_twitter_url = 'https://twitter.com/marswxreport?lang=en'
 
     # Retrieve page with the requests module
     browser = Browser('chrome', **executable_path, headless=False)
     browser.visit(mars_twitter_url)
+
+    # Add a 2 second sleep timer to allow the browser to finish loading the URL
+    time.sleep(2)
 
     # Get the HTML content of the visited page
     mars_twitter_html = browser.html
@@ -113,9 +132,9 @@ def scrape():
                 # Remove the "InSight" word from the tweet text
                 mars_weather = mars_weather.replace("InSight ", "")
                 # Print the weather tweet and break from loop
-                print("Here is the latest weather report tweet from Mars Twitter")
+                print("Scraped the latest weather report tweet from Mars Twitter:")
                 print("----------------------------------------------------------")
-                print(mars_weather)
+                print(mars_weather + "\n")
                 break
             else:
                 # Continue looping until we find the weather tweet
@@ -123,12 +142,15 @@ def scrape():
         except Exception as e:
             print(e)
 
+    # Store the weather tweet text into a dictionary
     mars_scraped_data["mars_weather"] = mars_weather
 
     # ------------------------------------------------------------------------------------------------
     # Visit the Mars Facts webpage
     # use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
     # ------------------------------------------------------------------------------------------------
+
+    print("Now Scraping Mars Facts...Hang Tight!")
 
     # URL of Mars Facts webpage to be scraped
     mars_facts_url = 'https://space-facts.com/mars/'
@@ -145,12 +167,20 @@ def scrape():
 
     # Set the index to the Description column and print to verify
     mars_facts_df.set_index('Description', inplace=True)
+    #mars_scraped_data["mars_facts"] = mars_facts_df.to_dict('index')
+    print("Found these Mars Facts...\n")
     print(mars_facts_df)
-    mars_scraped_data["mars_facts"] = mars_facts_df.to_dict('index')
-    
+
+    # Iterate over DataFrame and store Mars Facts into a dictionary
+    for i, row in mars_facts_df.iterrows():
+        i = i[:-1]
+        mars_scraped_data[i] = row[0]
+
     # -------------------------------------------------------------------------------------------------
     # Visit the [USGS Astrogeology site to obtain high resolution images for each of Mars' Hemispheres.
     # -------------------------------------------------------------------------------------------------
+
+    print("\nNow Scraping USGS Astrogeology Site for High-Res Mars Hemipshere Images...Hang Tight!")
 
     # URL of USGS Astrogeology site to be scraped
     usgs_astrogeo_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
@@ -158,6 +188,9 @@ def scrape():
     # Retrieve page with the requests module
     browser = Browser('chrome', **executable_path, headless=False)
     browser.visit(usgs_astrogeo_url)
+
+    # Add a 2 second sleep timer to allow the browser to finish loading the URL
+    time.sleep(2)
 
     # Dictionary and List to hold the image URLs and Titles of Mars' Hemispheres
     mars_hemisphere_image_dict = {}
@@ -195,9 +228,12 @@ def scrape():
         except Exception as e:
             print(e)
 
+    print("FINISHED Web Scraping...Here's all the Mars data formatted as JSON")
+    print("------------------------------------------------------------------\n")
+
+    # Save the hemisphere dict into the main dictionary that stores all scrapted data
     mars_scraped_data["mars_hemispheres"] = mars_hemisphere_image_urls
 
-    return mars_scraped_data
+    print(json.dumps(mars_scraped_data, indent=4))
 
-print("\n\n MARS SCRAPED DATA")
-print(scrape())
+    return (mars_scraped_data)
